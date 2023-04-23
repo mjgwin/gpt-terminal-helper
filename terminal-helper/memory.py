ebe773301199b35c1ddb48785f76b6713fc665d1
mjgwin <micahjgwin@gmail.com>
2023-04-22 21:42:32 -0500

    adding more parsing logic for the response

--- terminal-helper/memory.py
+++ terminal-helper/memory.py
@@ -19,7 +19,7 @@ def create_if_empty():
     
     
 def add_conversation(role, content):
-    cursor.execute("INSERT INTO memory VALUES(?, ?, ?)", (role, content, str(datetime.now())))
+    cursor.execute("INSERT INTO memory VALUES(?, ?, ?)", (role, json.dumps(content), str(datetime.now())))
     connection.commit()
     if mem_debug:
         term_print("Added conversation to memory", "cyan")
@@ -35,7 +35,7 @@ def get_recent_conversations():
     return conversations
 
 def print_memory():
-    term_print("================= Memory Contents Below =================")
+    term_print("================= Memory Contents Below =================", "cyan")
     for row in cursor.execute("SELECT role, content, timestamp FROM memory ORDER BY timestamp DESC"):
         term_print(json.dumps({"role": row[0], "content": row[1], "timestamp": row[2]}, indent=4), "cyan")
         
@@ -45,3 +45,11 @@ def write_memory_to_file(file_name):
         conversations.append({"role": row[0], "content": row[1], "timestamp": row[2]}, "cyan")
     with open(f"{file_name}.json", "w") as fp:
         fp.write(json.dumps(conversations, indent=4))
+    term_print(f"Wrote memory to file {file_name}", "cyan")
+        
+def clear_memory():
+    cursor.execute("DROP TABLE IF EXISTS memory")
+    term_print("Clearing memory", "cyan")
+    connection.commit()
+    create_if_empty()
+    
--- terminal-helper/terminal-helper.py
+++ terminal-helper/terminal-helper.py
@@ -3,6 +3,7 @@ import sys
 import re
 import subprocess
 import platform
+import json
 
 from io import StringIO
 from contextlib import redirect_stdout
@@ -18,8 +19,37 @@ import memory
 
 colorama.init()
 
-def term_print(input, color):
-    print(colored(f"terminal-helper > {input}", color))
+term_debug = True
+
+def term_print(output_text, color):
+    print(colored(f"terminal-helper > {output_text}", color))
+    
+def term_input(input_text, color):
+    return input(colored(f"terminal-helper > {input_text}", color))
+
+def is_valid_command(command_info):
+    keys = command_info.get("arguments", {}).keys()
+    return command_info.get("command", "") != "" and "body" in keys 
+    
+def run_python(arguments):
+    body = arguments.get("body", "")
+    if body:
+        stdout_new = StringIO()
+        with redirect_stdout(stdout_new):
+            exec(body)
+        term_print(f"Execute python done with response: {stdout_new.getvalue()}", "cyan")
+        
+def process_message(commands):
+    if not commands:
+        return
+    for command_info in commands:
+        if is_valid_command(command_info):
+            command = command_info.get("command", "")
+            arguments = command_info.get("arguments", {})
+            if command == "run_python":
+                run_python(arguments)
+        else:
+            term_print(f"Error, unable to process command: {json.dumps(command_info)}")
 
 operating_system = platform.platform()
 
@@ -46,13 +76,17 @@ Carefully think about which of the following commands will help the user accompl
 Your currently supported and allowed commands are: {str(ALLOWED_COMMANDS)}. \n
 The command format is: {str(COMMAND_FORMAT)}. \n
 You will provide a list of commands in your response to complete the objective.
+DO NOT ADD ANY OTHER COMMENTS BESIDES THE COMMANDS SO THE JSON IS ABLE TO BE PARSED
 Each command will have the needed arguments with additional information for the objective.
 The allowed arguments for each command are mapped below:
 {str(ALLOWED_ARGUMENTS_MAP)}
+PYTHON CODE MUST HAVE AN OUTPUT TO INFORM THE USER OF ITS RESULT.
 The mandatory response format must be valid JSON with no other text allowed. It must be formatted like below: \n
 {str(RESPONSE_FORMAT)} \n
-DO NOT INCLUDE EXTRA TEXT BEFORE OR AFTER THE RESPONSE.
+DO NOT INCLUDE ANY EXTRA TEXT BEFORE OR AFTER THE RESPONSE.
 """
+
+HELP_MESSAGE = "Commands include: \n > (clear_mem: Clear internal memory) \n > (print_mem: Display internal memory) \n > (quit: Exit application) \n > (write_mem: Write memory to log file)"
 
 if __name__ == "__main__":
     
@@ -67,6 +101,8 @@ if __name__ == "__main__":
 
     term_print(f"Working directory is {work_dir}", "cyan")
     
+    memory.create_if_empty()
+    
     try:
         os.chdir(work_dir)
     except FileNotFoundError:
@@ -75,10 +111,24 @@ if __name__ == "__main__":
     
     while running:
         
-        objective = input(colored("terminal-helper > Enter objective, help for more commands: ", "cyan"))
+        objective = term_input("Enter objective, help for more commands: ", "cyan")
         
         if objective == "quit":
             break
+        elif objective == "help":
+            term_print(HELP_MESSAGE, "cyan")
+            continue
+        elif objective == "clear_mem":
+            memory.clear_memory()
+            continue
+        elif objective == "print_mem":
+            memory.print_memory()
+            continue
+        elif objective == "clear_mem":
+            memory.write_memory_to_file("log.txt")
+            continue
+        
+        memory.add_conversation("user", objective)
         
         with Spinner():
             try:
@@ -89,10 +139,37 @@ if __name__ == "__main__":
                         {"role": "user", "content": f"OBJECTIVE:{objective}"},
                         {"role": "user", "content": f"CONTEXT:\n{memory.get_recent_conversations()}"},
                         {"role": "user", "content": f"INSTRUCTIONS:\n{INSTRUCTIONS}"},
-                    ])
+                    ],
+                    max_tokens=1000
+                )
             except Exception as e:
                 term_print("Error accessing the OpenAI API: " + str(e), "red")
+                response = {"error": "unable to access api"}
                 sys.exit(0)
         
+        print("")
+        
+        
+        try:
+            response_message = response['choices'][0]['message']['content']
+        except Exception as e:
+            response_message = {"error": "unable to access api"}
+            
+        parsed_response = response_message.replace("```", "").strip()
+        #TODO Parse response so json loads can work
+        print(f"Before: {parsed_response}")
+        response_message = json.loads(parsed_response)
+        commands = response_message.get("commands", [])
+        for command in commands:
+            command["arguments"] = eval(command.get("arguments", {}))
+        
+        if term_debug:
+            term_print(f"Response: {json.dumps(response_message)}", "cyan")
+         
+        if "error" not in response_message.keys():
+            memory.add_conversation("assistant", response_message)
+            process_message(commands)
+            
+        
 
-        term_print("Thanks for using terminal-helper!", "cyan")
+    term_print("Thanks for using terminal-helper!", "cyan")
